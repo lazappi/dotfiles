df5fea31dac5bf571a8c48edc1e54fe5f4362350
Luke Zappia <lazappi@users.noreply.github.com>
2019-01-30 12:28:47 +1100

    Turn off history sharing

--- source/50_prompt.sh
+++ source/50_prompt.sh
@@ -193,7 +193,7 @@ function prompt_command() {
 
 PROMPT_COMMAND="prompt_command"
 # Share history across windows
-PROMPT_COMMAND="history -a; history -c; history -r; $PROMPT_COMMAND"
+# PROMPT_COMMAND="history -a; history -c; history -r; $PROMPT_COMMAND"
 
 # Trim number of directory levels shown
 PROMPT_DIRTRIM=3
